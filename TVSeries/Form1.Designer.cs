10c4d31d88b883c7a3054e9748e8f6addf859b53
Berceanu Stefan-Adrian <sa.berceanu@gmail.com>
2017-01-14 21:37:33 +0200

    .

--- TVSeries/Form1.Designer.cs
+++ TVSeries/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TVSeries
 {
-    partial class Form1
+    partial class Form
     {
         /// <summary>
         /// Required designer variable.
@@ -29,11 +29,253 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblYear = new System.Windows.Forms.Label();
+            this.lblSeasons = new System.Windows.Forms.Label();
+            this.lblFinished = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtYear = new System.Windows.Forms.TextBox();
+            this.txtSeasons = new System.Windows.Forms.TextBox();
+            this.txtFinished = new System.Windows.Forms.TextBox();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.panel = new System.Windows.Forms.Panel();
+            this.tVSeriesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.yearDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.seasonsDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.finishedDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tVSeriesBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(6, 22);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Name";
+            // 
+            // lblYear
+            // 
+            this.lblYear.AutoSize = true;
+            this.lblYear.Location = new System.Drawing.Point(6, 52);
+            this.lblYear.Name = "lblYear";
+            this.lblYear.Size = new System.Drawing.Size(29, 13);
+            this.lblYear.TabIndex = 0;
+            this.lblYear.Text = "Year";
+            // 
+            // lblSeasons
+            // 
+            this.lblSeasons.AutoSize = true;
+            this.lblSeasons.Location = new System.Drawing.Point(6, 82);
+            this.lblSeasons.Name = "lblSeasons";
+            this.lblSeasons.Size = new System.Drawing.Size(48, 13);
+            this.lblSeasons.TabIndex = 0;
+            this.lblSeasons.Text = "Seasons";
+            // 
+            // lblFinished
+            // 
+            this.lblFinished.AutoSize = true;
+            this.lblFinished.Location = new System.Drawing.Point(6, 112);
+            this.lblFinished.Name = "lblFinished";
+            this.lblFinished.Size = new System.Drawing.Size(46, 13);
+            this.lblFinished.TabIndex = 0;
+            this.lblFinished.Text = "Finished";
+            // 
+            // txtName
+            // 
+            this.txtName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtName.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.tVSeriesBindingSource, "Name", true));
+            this.txtName.Location = new System.Drawing.Point(66, 19);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(522, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtYear
+            // 
+            this.txtYear.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtYear.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.tVSeriesBindingSource, "Year", true));
+            this.txtYear.Location = new System.Drawing.Point(66, 49);
+            this.txtYear.Name = "txtYear";
+            this.txtYear.Size = new System.Drawing.Size(152, 20);
+            this.txtYear.TabIndex = 2;
+            // 
+            // txtSeasons
+            // 
+            this.txtSeasons.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtSeasons.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.tVSeriesBindingSource, "Seasons", true));
+            this.txtSeasons.Location = new System.Drawing.Point(66, 79);
+            this.txtSeasons.Name = "txtSeasons";
+            this.txtSeasons.Size = new System.Drawing.Size(152, 20);
+            this.txtSeasons.TabIndex = 3;
+            // 
+            // txtFinished
+            // 
+            this.txtFinished.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtFinished.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.tVSeriesBindingSource, "Finished", true));
+            this.txtFinished.Location = new System.Drawing.Point(66, 109);
+            this.txtFinished.Name = "txtFinished";
+            this.txtFinished.Size = new System.Drawing.Size(152, 20);
+            this.txtFinished.TabIndex = 4;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AutoGenerateColumns = false;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nameDataGridViewTextBoxColumn,
+            this.yearDataGridViewTextBoxColumn,
+            this.seasonsDataGridViewTextBoxColumn,
+            this.finishedDataGridViewTextBoxColumn});
+            this.dataGridView.DataSource = this.tVSeriesBindingSource;
+            this.dataGridView.Location = new System.Drawing.Point(12, 170);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(600, 231);
+            this.dataGridView.TabIndex = 5;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(537, 407);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(456, 407);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 7;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(375, 407);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 8;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(294, 407);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(75, 23);
+            this.btnNew.TabIndex = 9;
+            this.btnNew.Text = "New";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // panel
+            // 
+            this.panel.Controls.Add(this.txtName);
+            this.panel.Controls.Add(this.lblName);
+            this.panel.Controls.Add(this.lblYear);
+            this.panel.Controls.Add(this.lblSeasons);
+            this.panel.Controls.Add(this.lblFinished);
+            this.panel.Controls.Add(this.txtYear);
+            this.panel.Controls.Add(this.txtFinished);
+            this.panel.Controls.Add(this.txtSeasons);
+            this.panel.Location = new System.Drawing.Point(12, 12);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(600, 152);
+            this.panel.TabIndex = 10;
+            // 
+            // tVSeriesBindingSource
+            // 
+            this.tVSeriesBindingSource.DataSource = typeof(TVSeries);
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            this.nameDataGridViewTextBoxColumn.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.nameDataGridViewTextBoxColumn.Width = 377;
+            // 
+            // yearDataGridViewTextBoxColumn
+            // 
+            this.yearDataGridViewTextBoxColumn.DataPropertyName = "Year";
+            this.yearDataGridViewTextBoxColumn.HeaderText = "Year";
+            this.yearDataGridViewTextBoxColumn.Name = "yearDataGridViewTextBoxColumn";
+            this.yearDataGridViewTextBoxColumn.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.yearDataGridViewTextBoxColumn.Width = 60;
+            // 
+            // seasonsDataGridViewTextBoxColumn
+            // 
+            this.seasonsDataGridViewTextBoxColumn.DataPropertyName = "Seasons";
+            this.seasonsDataGridViewTextBoxColumn.HeaderText = "Seasons";
+            this.seasonsDataGridViewTextBoxColumn.Name = "seasonsDataGridViewTextBoxColumn";
+            this.seasonsDataGridViewTextBoxColumn.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.seasonsDataGridViewTextBoxColumn.Width = 60;
+            // 
+            // finishedDataGridViewTextBoxColumn
+            // 
+            this.finishedDataGridViewTextBoxColumn.DataPropertyName = "Finished";
+            this.finishedDataGridViewTextBoxColumn.HeaderText = "Finished";
+            this.finishedDataGridViewTextBoxColumn.Name = "finishedDataGridViewTextBoxColumn";
+            this.finishedDataGridViewTextBoxColumn.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.finishedDataGridViewTextBoxColumn.Width = 60;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(624, 442);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.dataGridView);
+            this.MaximizeBox = false;
+            this.Name = "Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TV Series";
+            this.Load += new System.EventHandler(this.Form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.panel.ResumeLayout(false);
+            this.panel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tVSeriesBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblYear;
+        private System.Windows.Forms.Label lblSeasons;
+        private System.Windows.Forms.Label lblFinished;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtYear;
+        private System.Windows.Forms.TextBox txtSeasons;
+        private System.Windows.Forms.TextBox txtFinished;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.BindingSource tVSeriesBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn yearDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn seasonsDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn finishedDataGridViewTextBoxColumn;
     }
 }
 
--- TVSeries/Form1.cs
+++ TVSeries/Form1.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -10,11 +12,79 @@ using System.Windows.Forms;
 
 namespace TVSeries
 {
-    public partial class Form1 : Form
+    public partial class Form : System.Windows.Forms.Form
     {
-        public Form1()
+        TVSeriesEntities tvse;
+
+        public Form()
         {
             InitializeComponent();
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                panel.Enabled = true;
+                TVSeries tv = new TVSeries();
+                tvse.TVSeries.Add(tv);
+                tVSeriesBindingSource.Add(tv);
+                tVSeriesBindingSource.MoveLast();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            panel.Enabled = true;
+            txtName.Focus();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            panel.Enabled = false;
+            tVSeriesBindingSource.ResetBindings(false);
+
+            foreach(DbEntityEntry entry in tvse.ChangeTracker.Entries())
+            {
+                switch(entry.State)
+                {
+                    case EntityState.Added:
+                        entry.State = EntityState.Detached;
+                        break;
+                    case EntityState.Modified:
+                        entry.State = EntityState.Unchanged;
+                        break;
+                    case EntityState.Deleted:
+                        entry.Reload();
+                        break;
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                tVSeriesBindingSource.EndEdit();
+                tvse.SaveChangesAsync();
+                panel.Enabled = false;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                tVSeriesBindingSource.ResetBindings(false);
+            }
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            panel.Enabled = false;
+            tvse = new TVSeriesEntities();
+            tVSeriesBindingSource.DataSource = tvse.TVSeries.ToList();
         }
     }
 }
